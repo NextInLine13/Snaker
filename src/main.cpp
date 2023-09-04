43dc433b9a8c34792daabb3e777097dc7c9a4b0d
Michal Bakiewicz <michal.bakiewicz@huuugegames.com>
2023-09-04 21:19:39 +0200

    Add input handling.

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,23 @@
 #include <memory>
 #include <iostream>
 
+enum class Direction
+{
+    Left,
+    Right,
+    Up,
+    Down
+};
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
     const int blockSize = 40;
-    const int updateInterval = 1;
+    constexpr int screenWidth = 10 * blockSize;
+    constexpr int screenHeight = 10 * blockSize;
+
+    const float updateInterval = 0.75;
 
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
@@ -38,20 +46,42 @@ int main(void)
 
     auto time = GetTime();
     Vector2 snakePosition = { 200, 200 };
+    Vector2 deltaPosition = { blockSize, 0 };
+    Direction snakeDirection = Direction::Right;
+
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        //handle input
+        if (snakeDirection != Direction::Left && IsKeyDown(KEY_RIGHT))
+        {
+            snakeDirection = Direction::Right;
+        }
+        if (snakeDirection != Direction::Right && IsKeyDown(KEY_LEFT))
+        {
+            snakeDirection = Direction::Left;
+        }
+        if (snakeDirection != Direction::Down && IsKeyDown(KEY_UP))
+        {
+            snakeDirection = Direction::Up;
+        }
+        if (snakeDirection != Direction::Up && IsKeyDown(KEY_DOWN))
+        {
+            snakeDirection = Direction::Down;
+        }
+
+
         auto newTime = GetTime();
         if (newTime - time > updateInterval )
         {
             time = newTime;
-
+            snakePosition.x += deltaPosition.x;
+            snakePosition.y += deltaPosition.y;
+        }
             BeginDrawing();
                 ClearBackground(RAYWHITE);
                 DrawRectangleV( snakePosition, { blockSize, blockSize }, BLACK );
-                snakePosition.x += blockSize;
-
             EndDrawing();
-        }
+        
     }
     CloseWindow();
 
