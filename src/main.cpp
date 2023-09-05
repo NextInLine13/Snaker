83cabde404fca889da25e4bc09bacde8b7254a42
Michal Bakiewicz <michal.bakiewicz@huuugegames.com>
2023-09-05 20:08:22 +0200

    Handle direction changing.

--- src/main.cpp
+++ src/main.cpp
@@ -28,25 +28,38 @@ enum class Direction
     Down
 };
 
+Vector2 GetUpdatePositionVector( Direction direction, float size )
+{
+    if (direction == Direction::Left )
+    {
+        return { -size, 0 };
+    }
+    else if (direction == Direction::Right)
+    {
+        return { size, 0 };
+    }
+    else if (direction == Direction::Up)
+    {
+        return { 0, -size };
+    }
+    else if (direction == Direction::Down)
+    {
+        return { 0, size };
+    }
+}
+
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int blockSize = 40;
     constexpr int screenWidth = 10 * blockSize;
     constexpr int screenHeight = 10 * blockSize;
-
     const float updateInterval = 0.75;
 
-
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
-
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
     auto time = GetTime();
     Vector2 snakePosition = { 200, 200 };
-    Vector2 deltaPosition = { blockSize, 0 };
     Direction snakeDirection = Direction::Right;
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -69,11 +82,11 @@ int main(void)
             snakeDirection = Direction::Down;
         }
 
-
         auto newTime = GetTime();
         if (newTime - time > updateInterval )
         {
             time = newTime;
+            auto deltaPosition = GetUpdatePositionVector( snakeDirection, blockSize );
             snakePosition.x += deltaPosition.x;
             snakePosition.y += deltaPosition.y;
         }
@@ -81,7 +94,6 @@ int main(void)
                 ClearBackground(RAYWHITE);
                 DrawRectangleV( snakePosition, { blockSize, blockSize }, BLACK );
             EndDrawing();
-        
     }
     CloseWindow();
 
